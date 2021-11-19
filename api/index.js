583f0ae789bb81c4a51b4bbbd089a9444172bd5a
biancamagalhaes <bianca.magalhaes2610@gmail.com>
2021-11-19 15:28:56 -0300

    remove unused session and add new database url

--- api/index.js
+++ api/index.js
@@ -5,17 +5,6 @@ const port = 5001;
 const dotenv = require('dotenv');
 dotenv.config();
 
-app.use(session({
-  store: new pgSession({
-    conString: 'postgres://6d2d9670fc15dd99d233a0bb007dae891dcd2962cd523faf36d76d8b297b3354@ec2-44-198-223-154.compute-1.amazonaws.com:5432/daa3sd4s3t6rnh',
-  }),
-  key: 'kwnfprkjxqpmmi',
-  secret: '6d2d9670fc15dd99d233a0bb007dae891dcd2962cd523faf36d76d8b297b3354',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 10 * 10 * 6000000 },
-}));
-
 app.use('/user', require('./route/user'));
 app.use('/psychologist', require('./route/psychologist'));
 app.use('/diary', require('./route/diary'));
